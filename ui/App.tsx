50dd6b7271b9e2886c6d43151780679bd17af325
Greg Brimble <developer@gregbrimble.com>
2020-12-23 15:46:16 +0000

    Format test

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,17 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import React, { FC } from "react";
-import { View } from "react-native";
-import { DripsyProvider, styled, Text, Button } from "dripsy";
+import { StatusBar } from 'expo-status-bar'
+import React, { FC } from 'react'
+import { View } from 'react-native'
+import { DripsyProvider, styled, Text, Button } from 'dripsy'
 
-import { theme } from "./theme";
+import { theme } from './theme'
 
 // TODO: Fix H1 etc.
 
 const Container = styled(View)({
   flex: 1,
-  bg: ["white", "blue-600"],
-  margin: 4
-});
+  bg: ['white', 'blue-600'],
+  margin: 4,
+})
 
 // const MyButton = styled(Button)({
 //   bg: ["white"],
@@ -25,8 +25,8 @@ export const App: FC = () => {
       <Container sx={{ height: 100 }}>
         <Text
           sx={{
-            width: ["1/2", "full"],
-            height: 100
+            width: ['1/2', 'full'],
+            height: 100,
           }}
         >
           Open up App.tsx to start working on your app!
@@ -35,9 +35,9 @@ export const App: FC = () => {
       </Container>
       <Button
         title="Click Me"
-        onPress={() => alert("hi")}
-        sx={{ backgroundColor: "red-600" }}
+        onPress={() => alert('hi')}
+        sx={{ backgroundColor: 'red-600' }}
       />
     </DripsyProvider>
-  );
-};
+  )
+}
